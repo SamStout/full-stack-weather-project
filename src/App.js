fa8a83ad1d44de7e4e025b35c14cd5659b4feb52
samiistout <samuelstout6580@gmail.com>
2022-09-08 14:40:56 -0500

    Before Gabe

--- src/App.js
+++ src/App.js
@@ -1,22 +1,49 @@
-import logo from './logo.svg';
+import { useState, useEffect, useRef } from 'react';
 import './App.css';
+import ApiDisplay from './components/ApiDisplay';
 
 function App() {
+  const [weather, setWeather] = useState()
+  
+
+
+  const getWeather = (input)=>{
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=b9d30726a834b3a44eec4e5a08ad64bd`)
+    .then((res)=>{
+      return res.json()
+    })
+    .then ((forecast)=>{
+      if (forecast.cod == 404) {
+        console.log("not a city")
+        console.log(forecast.name +" not city")
+      } else if (forecast.cod == "400") {
+        console.log("not found")
+        console.log(forecast.name +" notfound")
+      }
+      else setWeather(forecast)
+    }) 
+    .catch ((err) => {
+      console.log(err.response.data);
+      this.setState({ error: err.response.data.message });
+    })
+  }
+
+//happens on mount/pageload
+console.log(weather)
+
+const handleInput = event =>{
+  getWeather(event.target.value)
+}
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Weather-Project</h1>
+        <input 
+          className='inputBox' 
+          onInput={handleInput} 
+        ></input>
+        {weather && <ApiDisplay weather = {weather}/>}
       </header>
     </div>
   );
---
+++ src/components/ApiDisplay.jsx
@@ -0,0 +1,40 @@
+
+const ApiDisplay = (props) =>{
+    const {weather} = props;
+
+    const current = new Date();
+    const date = `${current.getDate()}/${current.getMonth()+1}/${current.getFullYear()}`;
+
+    const weatherToApi = {
+        forecast: {
+        'city' : `${weather.name}`,
+        'date' : `${date}`,
+        'weather':`${weather.weather[0].description}`
+    }}
+    
+    const addWeatherToApi = () =>{
+            fetch("http://192.168.254.145:3080", {
+            method: 'POST',
+            mode: 'cors',
+            headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(weatherToApi)
+            })
+          // .then (
+          //   {getNewAPI}
+          //   )
+    }
+
+return (
+    <div>
+       {weather && <h2>{weather.name}</h2>}
+       {weather &&<h2>{weather.clouds.all}</h2>}
+       {weather &&<h2>{weather.weather[0].description}</h2>}
+       {date}
+    </div>
+)
+}
+
+export default ApiDisplay;
